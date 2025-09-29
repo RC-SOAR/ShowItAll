4ca60c54f3b19a4ff29af6e5111b79d95a39f23f
Mike Shellim <m.shellim@gmail.com>
2025-09-30 00:11:14 +0100

    Initial commit v0.9.18

---
+++ tools/package.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal
+
+:: Set up variables
+set "root=D:\OneDrive\RC\LuaDev\Showall"
+set "tempdir=%root%\Tools\temp"
+set "zipname=ShowItAll_09.zip"
+set "zipoutput=%root%\package\%zipname%"
+
+:: Clean up any previous temp folder
+if exist "%tempdir%" rd /s /q "%tempdir%"
+
+:: Copy selected files into temp directory
+xcopy "%root%\src\*.lua" "%tempdir%\"
+xcopy "%root%\docs\*.pdf" "%tempdir%\"
+
+:: Compress the folder using PowerShell
+powershell -command "Compress-Archive -Path '%tempdir%\*' -DestinationPath '%zipoutput%' -Force"
+
+:: Delete the temporary folder after compression
+rd /s /q "%tempdir%"
+
+echo Zip archive complete
+pause
